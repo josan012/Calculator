862ef92dd04ef183543903b5fbcaeb7f00973b91
josan012 <mihai.josan12@gmail.com>
2024-03-24 12:03:07 +0200

    improved code

--- src/providers/NumberProvider.tsx
+++ src/providers/NumberProvider.tsx
@@ -1,82 +1,60 @@
-import React, { KeyboardEvent, ReactElement, useState } from 'react';
+import React, { ReactElement, useState } from "react";
 
 interface ContextProps {
     input: string;
-    reset: () => void;
-    solve: (expression: string) => void;
-    remove: () => void;
-    handleInput: (inputValue: string | Event) => void;
-};
+    handleInput: (inputValue: string) => void;
+}
 
 export const NumberContext = React.createContext<ContextProps>({
     input: "",
-    reset: () => { },
-    solve: () => { },
-    remove: () => { },
     handleInput: () => { },
-},
-);
+});
 
 interface Props {
     children: ReactElement;
 }
 
 const NumberProvider: React.FC<Props> = ({ children }) => {
-
-    const [input, setInput] = useState("")
+    const [input, setInput] = useState("");
 
     const reset = () => {
-        setInput("")
-    }
-
-    const remove = () => {
-        setInput(input.substring(0, input.length - 1))
-    }
-
-    const solve = (input: string) => {
-        eval(input);
-    }
-
-    const handleInput = (inputValue: string | KeyboardEvent) => {
-        if (typeof inputValue === 'string') {
-            const value = inputValue;
-            if (value === '=') {
-                solve(input);
-            } else if (value === 'Clear') {
-                reset();
-            } else if (value === 'Remove') {
-                remove();
-            } else {
-                setInput(prevInput => prevInput + value);
-            }
-        } else {
-            const event = inputValue;
-            const { key } = event;
-            if (/^[0-9+\-*/.()]$/.test(key)) {
-                setInput(prevInput => prevInput + key);
-            } else if (key === 'Enter') {
-                solve(input);
-            } else if (key === 'Escape') {
-                reset();
-            } else if (key === 'Backspace') {
-                remove();
-            }
-        }
+        setInput("");
     };
 
-    document.addEventListener('keydown', (event: Event) => {
-        if (event instanceof KeyboardEvent) {
-            handleInput(event);
-        }
-    });
+    const remove = () => {
+        setInput(input.substring(0, input.length - 1));
+    };
 
+    const solve = (input: string) => {
+        const result = eval(input);
+        setInput(result.toString());
+    };
+
+    const handleInput = (value: string): void => {
+        switch (value) {
+            case "=":
+            case "Enter":
+                solve(input);
+                break;
+            case "Clear":
+            case "Escape":
+                reset();
+                break;
+            case "Remove":
+            case "Backspace":
+                remove();
+                break;
+            default:
+                if (/^[0-9+\-*/.()]$/.test(value)) {
+                    setInput(prevInput => prevInput + value);
+                }
+                break;
+        }
+    };
 
 
     const contextValue: ContextProps = {
         input,
-        reset,
-        solve,
-        remove,
         handleInput,
     };
 
